1e165eb59ac10d43e05f95450ba657e7facade3f
sakthikumaran-04 <142299178+sakthikumaran-04@users.noreply.github.com>
2023-11-18 15:55:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    document.getElementById("menu").style.display="block";
+    document.getElementById("main").style.filter="blur(5px)";
+}
+function hideMenu(){
+    document.getElementById("menu").style.display="none";
+    document.getElementById("main").style.filter="blur(0px)";
+}
